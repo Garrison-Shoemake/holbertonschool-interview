db71b54028efc12e218c6a1ae4ddf41906185760
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-11-05 18:59:09 +0000

    spaces not tabs for pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" This is a lockbox interview question and determines if all boxes can be opened"""
+""" This lockbox interview question determines if all boxes can be opened"""
 
 
 def canUnlockAll(boxes):
-	""" Determines if all boxes can be unlocked or not """
-	keyring = [0]
-	if boxes is None or len(boxes) == 0:
-		return False
+    """ Determines if all boxes can be unlocked or not """
+    keyring = [0]
+    if boxes is None or len(boxes) == 0:
+        return False
 
-	for keys in keyring:
-		for key in boxes[keys]:
-			if key not in keyring and key < len(boxes):
-				keyring.append(key)
-		
-	return len(keyring) == len(boxes)
+    for keys in keyring:
+        for key in boxes[keys]:
+            if key not in keyring and key < len(boxes):
+                keyring.append(key)
+
+    return len(keyring) == len(boxes)
 
 # def addkey(keyring, newkeys):
 # 	for key in newkeys:
