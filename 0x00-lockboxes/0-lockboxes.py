37eff48e44ca60504f7e69cbc9f6743a6a5aff94
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-11-05 04:20:34 +0000

    progress up to missing 5 key in second main test

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" This is a lockbox interview question and determines if all boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+	keyring = [0]
+	if boxes is None or len(boxes) == 0:
+		return False
+
+	for key in keyring:
+		newkeys = boxes[key]
+		keyring = addkey(keyring, newkeys)
+
+	return len(keyring) == len(boxes)
+
+def addkey(keyring, newkeys):
+	for key in newkeys:
+		if key not in keyring:
+			keyring.append(key)
+			keyring.sort()
+		else:
+			pass
+	return keyring
