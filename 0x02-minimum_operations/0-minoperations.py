0b1d2acf6389495a48029ff20761e674d5b35e2e
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-11-25 01:46:07 +0000

    add task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+""" finds the minimum number of operations with copy and paste"""
+
+
+def minOperations(n):
+    """ defines minimum operations through infinite mod loop
+    adds results if mod equals zero
+    divides n by results until done
+    """
+    total = 0
+    i = 2
+    """ """
+
+    
+    if (n <= 0):
+        """ if n is impossible return 0"""
+
+        return 0
+
+    
+    while (i <= n ):
+        """ while i less than or equal to n - mod i into n"""
+
+        while (n % i == 0):
+            """ reduces n while adding i steps to total until done """
+            total += i
+            n = n / i
+        i += 1
+
+    return total
+    
