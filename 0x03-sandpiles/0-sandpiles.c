103d5e1a5c9660726b08229fed5a0a5b1d2c4dd6
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-12-06 03:25:35 +0000

    add betty checks

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -4,7 +4,6 @@
  * @grid1: first grid to add
  * @grid2: second grid to add
  */
-
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int x, y;
@@ -14,42 +13,40 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	for (x = 0; x < 3; x++)
 		for (y = 0; y < 3; y++)
 			grid1[x][y] += grid2[x][y];
-	
+
 	topple(grid1);
 }
 
-
 /**
  * topple - topples all necessary cells to neighbours
- * @grid: grid to topple and stabilize 
- * 
+ * @grid: grid to topple and stabilize
  */
 void topple(int grid[3][3])
 {
 	int x, y;
 
-	while(!stable(grid))
+	while (!stable(grid))
 	{
 		printf("=\n");
 		print_grid(grid);
-		
+
 		for (x = 0; x < 3; x++)
 		{
 			for (y = 0; y < 3; y++)
 			{
-				/* the following is to topple the piles on to neighbours */			
+				/* the following is to topple the piles on to neighbours */
 				if (grid[x][y] > 3)
 				{
 					grid[x][y] -= 4;
 					if (x < 2)
-						grid[x+1][y]++;
+						grid[x + 1][y]++;
 					if (x > 0)
-						grid[x-1][y]++;
+						grid[x - 1][y]++;
 					if (y < 2)
-						grid[x][y+1]++;
+						grid[x][y + 1]++;
 					if (y > 0)
-						grid[x][y-1]++;
-				}			
+						grid[x][y - 1]++;
+				}
 			}
 		}
 	}
@@ -57,7 +54,7 @@ void topple(int grid[3][3])
 
 /**
  * stable - boolean helper function for stability
- * @grid - grid to check for stability
+ * @grid: grid to check for stability
  * Return: returns either true or false
  */
 bool stable(int grid[3][3])
@@ -67,9 +64,9 @@ bool stable(int grid[3][3])
 	for (x = 0; x < 3; x++)
 		for (y = 0; y < 3; y++)
 			if (grid[x][y] > 3)
-				return false;
+				return (false);
 
-	return true;
+	return (true);
 }
 
 /**
@@ -79,16 +76,16 @@ bool stable(int grid[3][3])
  */
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
 }
