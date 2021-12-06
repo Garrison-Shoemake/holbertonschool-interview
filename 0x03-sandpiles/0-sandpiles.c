18f564473f1007dc915813c7cd35dfc0862287b4
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-12-06 00:53:20 +0000

    moved topple to new function because i cant do it all in one and still have same output

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -9,34 +9,54 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int x, y;
 
+
 	/* the following will be adding the two original piles together */
 	for (x = 0; x < 3; x++)
 		for (y = 0; y < 3; y++)
 			grid1[x][y] += grid2[x][y];
-
 	
+	while(!stable(grid1))
+	{
+		topple(grid1);
+		stable(grid1);
+	}
+}
+
+
+/**
+ * @brief 
+ * 
+ */
+void topple(int grid[3][3])
+{
+	int x, y;
+	int subgrid[3][3];
+
 	for (x = 0; x < 3; x++)
 	{
 		for (y = 0; y < 3; y++)
 		{
-				
-				/* the following is to topple the piles on to neighbours */			
-				if (grid1[x][y] > 3)
-				{
-					grid1[x][y] -= 4;
-					if (x+1 < 3)
-						grid1[x+1][y]++;
-					if (x-1 >= 0)
-						grid1[x-1][y]++;
-					if (y+1 < 3)
-						grid1[x][y+1]++;
-					if (y-1 >= 0)
-						grid1[x][y-1]++;
-				}
+			subgrid[x][y] = grid[x][y];
+
+
+			/* the following is to topple the piles on to neighbours */			
+			if (subgrid[x][y] > 3)
+			{
+				grid[x][y] -= 4;
+				if (x+1 < 3)
+					grid[x+1][y]++;
+				if (x-1 >= 0)
+					grid[x-1][y]++;
+				if (y+1 < 3)
+					grid[x][y+1]++;
+				if (y-1 >= 0)
+					grid[x][y-1]++;
+			}
 								
 		}
 	}
 }
+
 
 
 /**
--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -1,9 +1,10 @@
 #ifndef SANDPILES_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
+bool stable(int grid[3][3]);
+void topple(int grid[3][3]);
 
 #endif
