5c7916192cd68000d67fa2c48f68e38733c7a430
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-12-22 23:26:11 +0000

    task 0 file and header just in case

---
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks a list for if its a palindrome
+ * @head: head of the list to check
+ * Return: returns 0 if not and 1 if is
+ */
+int is_palindrome(listint_t **head)
+{
+    listint_t *node = *head;
+	listint_t *slow, *fast, *prev, *second_ll_start, *head2, *next, *start;     
+    slow = fast = prev = node; 
+    if ( node -> next == NULL) 
+        return 1;          
+      while ( slow && fast && fast -> next ) 
+    { 
+        prev = slow; 
+        slow = slow -> next; 
+        fast = fast -> next -> next;     
+	} 
+    if ( fast && fast -> next == NULL) 
+        start = prev -> next;     
+	else 
+        start = prev;     
+		start = start -> next; 
+	    prev -> next = NULL; 
+	    second_ll_start = NULL; 
+    while ( start != NULL) 
+    { 
+       next = start -> next; 
+       start -> next = second_ll_start;       	 
+		 second_ll_start = start; 
+        start = next;     
+	} 
+		head2 = second_ll_start;     
+		while ( head2 && head ) 
+    	{ 
+        if ( head2 -> n == node -> n ) 
+        { 
+            head2 = head2 -> next;             
+			node = node -> next;         
+		} else 
+            return 0;    	 
+        } 
+    if ( head2 == node ) 
+        return 1; 
+
+    return(0);
+}
