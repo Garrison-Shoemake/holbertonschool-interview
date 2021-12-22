183e554f08560b92fe0182e4181ea562483a4a63
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2021-12-22 23:41:52 +0000

    cleaned up code and added comments cause this one hurts my brain

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -9,18 +9,28 @@ int is_palindrome(listint_t **head)
     listint_t *node = *head;
 	listint_t *slow, *fast, *prev, *second_ll_start, *head2, *next, *start;     
     
+    /* empty lists are palindromes */
     if (!*head)
         return (1);
 
+    /* setting up for traversal */
     slow = fast = prev = node; 
+
+    /* single item lists are palindromes */
     if ( node -> next == NULL) 
         return 1;          
-      while ( slow && fast && fast -> next ) 
+    
+    /* while our single iterative and double exist and there is 
+    at least one more after, set prev to last one, iterate nodes*/
+    while ( slow && fast && fast -> next ) 
     { 
         prev = slow; 
         slow = slow -> next; 
         fast = fast -> next -> next;     
 	} 
+    /* if the second iterator reaches the end of the list
+    we want to start a second list for comparison, starting in 
+    the middle of the original list*/
     if ( fast && fast -> next == NULL) 
         start = prev -> next;     
 	else 
@@ -28,23 +38,27 @@ int is_palindrome(listint_t **head)
 		start = start -> next; 
 	    prev -> next = NULL; 
 	    second_ll_start = NULL; 
+    /* constructs the second list */
     while ( start != NULL) 
     { 
-       next = start -> next; 
-       start -> next = second_ll_start;       	 
-		 second_ll_start = start; 
+        next = start -> next; 
+        start -> next = second_ll_start;       	 
+	    second_ll_start = start; 
         start = next;     
 	} 
-		head2 = second_ll_start;     
-		while ( head2 && head ) 
-    	{ 
+    /* sets head2 to constructed second list
+    comaprison is done in the while loop after
+    so that if at any point they don't match, return false and zero */
+	head2 = second_ll_start;     
+	while ( head2 && head ) 
+    { 
         if ( head2 -> n == node -> n ) 
         { 
             head2 = head2 -> next;             
 			node = node -> next;         
 		} else 
             return 0;    	 
-        } 
+    } 
     if ( head2 == node ) 
         return 1; 
 
