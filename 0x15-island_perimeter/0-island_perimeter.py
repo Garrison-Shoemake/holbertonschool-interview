feeb43f390a96f62758d6e00620f1a86d35c7b37
Garrison-Shoemake <garrisonjunkandmore@gmail.com>
2022-05-11 20:26:27 +0000

    pep8 / pycodestyle

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Finds the perimeter of an island """
 
+
 def island_perimeter(grid):
     """ find the perimeter area of an island described through grid """
 
@@ -8,11 +9,16 @@ def island_perimeter(grid):
     r, c = len(grid), len(grid[0])
     for i in range(r):
         for j in range(c):
-            if grid[i][j] == 0: continue
+            if grid[i][j] == 0:
+                continue
             perimeter += 4
-            if i > 0: perimeter -= grid[i-1][j]
-            if j > 0: perimeter -= grid[i][j-1]
-            if i < r-1: perimeter -= grid[i+1][j]
-            if j < c-1: perimeter -= grid[i][j+1]
+            if i > 0:
+                perimeter -= grid[i-1][j]
+            if j > 0:
+                perimeter -= grid[i][j-1]
+            if i < r-1:
+                perimeter -= grid[i+1][j]
+            if j < c-1:
+                perimeter -= grid[i][j+1]
 
     return perimeter
